5537b0ef6a6211e1740e80b99ffcf7e1485815d1
manman4 <manchanr4@gmail.com>
2020-12-20 19:20:20 +0900

    １つの点から多くの枝に伸びている点は色を変えて、大きく表示

--- python/Networkx/grid.py
+++ python/Networkx/grid.py
@@ -28,8 +28,13 @@ for j in range(m):
         if (i,j) in G and (i+1,j) in G:
             G.add_edge((i, j), (i+1, j))
 
+#ノードから出ているエッジが多いほどノードの描画サイズを大きくする
+node_size = [10*len(G[i]) for i in G.nodes()]
+#ノードから出ているエッジが３つ以上であれば赤色でノードを描画する
+node_color = ['red' if len(G[i]) >= 3 else 'blue' for i in G.nodes()]
+
 pos = {n: (n[1], n[0]) for n in G.nodes()}
-nx.draw_networkx_nodes(G, pos, node_size=30, alpha=1, node_color='blue')
+nx.draw_networkx_nodes(G, pos, node_size=node_size, alpha=1, node_color=node_color)
 nx.draw_networkx_edges(G, pos, label=1, edge_color="black", width=2)
 plt.savefig('graph_sample.png')
 
