6cc260e9420db294939018ca4a69a2f1878bcdf3
manman4 <manchanr4@gmail.com>
2021-02-11 21:03:57 +0900

    上書き

--- python/RPA/gui.py
+++ python/RPA/gui.py
@@ -1,9 +1,12 @@
 import pyautogui as pgui
 import time
+import sys
 
 def gui():
     for i in range(10000):
-        print(str(i) + '分経過')
+        sys.stdout.write("\r{}分経過".format(i))
+        sys.stdout.flush()
+
         pgui.click(15, 15)
         time.sleep(100)
 gui()
