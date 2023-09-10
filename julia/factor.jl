b60084590dd0698b7f1845d5e28d18ac501f3159
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-11 00:34:08 +0900

    素数のリスト

--- julia/factor.jl
+++ julia/factor.jl
@@ -1,6 +1,7 @@
 using Primes
 
 
+
 n = 101
 is_prime_result = isprime(n)
 
@@ -11,6 +12,14 @@ else
 end
 
 
+
 n = 202112271521
 factors = factor(n)
 println("202112271521の素因数分解: $n = $factors")
+
+
+
+start_range = 1
+end_range = 1000
+primes_in_range = primes(start_range, end_range)
+println("範囲 $start_range から $end_range の素数: $primes_in_range")
