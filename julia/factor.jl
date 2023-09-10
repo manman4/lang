0aa32adddac1fd0bda7e1f4033b799ca52c68528
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-11 00:31:10 +0900

    Primes

--- julia/factor.jl
+++ julia/factor.jl
@@ -1,5 +1,16 @@
 using Primes
 
+
+n = 101
+is_prime_result = isprime(n)
+
+if is_prime_result
+    println("$n は素数です。")
+else
+    println("$n は素数ではありません。")
+end
+
+
 n = 202112271521
 factors = factor(n)
 println("202112271521の素因数分解: $n = $factors")
